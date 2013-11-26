59780ef275bbba47d8f3cfd3c5f864547493af7a
ayatsai <ctsai1011@hotmail.com>
2013-11-25 21:57:30 -0800

    improved comments

--- clue.pl
+++ clue.pl
@@ -5,24 +5,28 @@
  * Wesley Tsai     44396109
  */
  
-% Notes
-% documentation on the works
-% easy to use interface
+/*
+ * Notes
+ */
+ 
+/* documentation on the works */
+/* easy to use interface */
 
 /*
  * Set-Up
  */
 
-% dynamic function initialize
+/* dynamic function initialize */
 :- dynamic innocentroom/1.
 :- dynamic innocentweapon/1.
 :- dynamic innocentperson/1.
 :- dynamic suggestions/1.
 
 
-% Order of Play (whose turn next) - idk why we need this
+/* Order of Play (whose turn next) - idk why we need this */
 
 
+/* initialize game */
 % number of players
 % Cards On Hand
 setup(PlayerNum, COH) :- assert(playernum(PlayerNum)), innocent(COH).
@@ -57,41 +61,57 @@ validateperson("mrs. peacock").
  * Minimal GamePlay
  */
 
-% Track Suggestions
-%	- restrict usage
+/* See Database Contents */
+% implement for all as necessary
+
+/* Track Suggestions */
+
+% make suggestion
+% restrict usage
 suggest(ROOM, WEAPON, PERSON) :- 
 	validateroom(ROOM), 
 	validateweapon(WEAPON), 
 	validateperson(PERSON), 
 	assert(suggestions(ROOM, WEAPON, PERSON)).
+
+% output all suggestions of check if already suggested
 checksuggestion(ROOM, WEAPON, PERSON) :- suggestions(ROOM, WEAPON, PERSON).
 
-% Record Learned Data
+/* Record Learned Data */
+
 % Record innocent objects
-% 	- restrict usage
+% restrict usage
 innocent(NG) :- validateroom(NG), assert(innocentroom(NG)).
 innocent(NG) :- validateweapon(NG), assert(innocentweapon(NG)).
 innocent(NG) :- validateperson(NG), assert(innocentperson(NG)).
 
-% Make Accusations
-
-% See Database Contents
+% output known innocent objects
 seeinnocent(A) :- innocentroom(A).
 seeinnocent(A) :- innocentweapon(A).
 seeinnocent(A) :- innocentperson(A).
 
-/*
- * Advanced GamePlay
- */
- 
-% Suggestions Inferred by Other Players
-% Suggest Next Suggestion
-%	- based on location?
-% Suggest Card to Show
-%	- show already showed card
+/* Make Accusations */
 
-% Advance GamePlay
-% Assess How Close Others are to Winning
-%	- heuristics: known information count?
-% Advice Suggestions for Tricking Others 
-%	- make accusations of own card
+
+/*
+ * Intermediate GamePlay
+ */
+
+/* Suggestions Inferred by Other Players */
+% modify heuristics of each
+
+/* Suggest Next Suggestion */
+% based on location?
+
+/* Suggest Card to Show */
+% show already showed card to same player
+
+/*
+ * Intermediate GamePlay
+ */
+
+/* Assess How Close Others are to Winning */
+% heuristics: known information count?
+
+/* Advice Suggestions for Tricking Others  */
+% make accusations of own card
