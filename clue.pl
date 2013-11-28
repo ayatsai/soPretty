78abb467f12c804ab8bd935dfda29353d59eb1c3
ayatsai <ctsai1011@hotmail.com>
2013-11-28 00:16:33 -0800

    Finished opponent turn

--- clue.pl
+++ clue.pl
@@ -27,6 +27,8 @@
 :- dynamic knownCard/2.
 % list of suspects
 :- dynamic unknownCard/3.
+% list of shown cards to other players
+:- dynamic shownCard/2.
 
 
 clue :- setup, !, playGame.
@@ -62,7 +64,7 @@ getStartingPlayerTurnLoop(X) :-
 getStartingPlayerTurnLoop(X) :- 
 	X < 0, 
 	getStartingPlayerTurn.
-% valide player index, record
+% valid player index, record
 getStartingPlayerTurnLoop(X) :- 
 	setPlayerTurn(X).
 
@@ -108,20 +110,36 @@ playGame :- currentPlayer(X), checkState(X), !.
 
 % my turn
 % add playGame call
-checkState(X) :- X is 0, myTurn, nextPlayer(X).
+checkState(X) :- X =:= 0, myTurn, nextPlayer(X).
 
 % add playGame call
 % other player turn
-checkState(X) :- X =\= 0, oppGetSuggestion, nextPlayer(X).
+checkState(X) :- X =\= 0, oppGetSuggestion, nextPlayer(X), playGame.
 % get suggestion made by player if any
 oppGetSuggestion :- 
 	println('Did opponent make a suggestion?'), 
 	println('Enter done if no suggestions.'),
 	println('Otherwise input each card one by one'),
-	read(X), 
-	oppGetSuggestionLoop1(X, 3).
+	read(X),
+	oppGetSuggestionCheck(X).
+% check if player suggested
+oppGetSuggestionCheck(done).
+oppGetSuggestionCheck(X) :- oppRecordSuggestionLoop(X, 3).
 % record the three objects suggested
-oppGetSuggestionLoop(X, I) :- oppUpdateHeuristics(X), IN is I - 1, oppGetSuggestionLoop(X, IN).
+oppRecordSuggestionLoop(X, I) :- I is 1, incHeuristics(X), oppGetShownCard.
+oppRecordSuggestionLoop(X, I) :- card(X), incHeuristics(X), read(Y), IN is I - 1, oppRecordSuggestionLoop(Y, IN).
+oppRecordSuggestionLoop(_, I) :- read(Y), oppRecordSuggestionLoop(Y, I).
+
+oppGetShownCard :- 
+	println('Did you show a card?'),
+	println('If not, input done.'),
+	println('Else, input card.'),
+	read(X),
+	oppRecordShownCard(X).
+
+oppRecordShownCard(done).
+oppRecordShownCard(X) :- card(X), addShownCard(X).
+oppRecordShownCard(_) :- read(Y), oppRecordShownCard(Y).
 
 
 /* Game Mechanics */
@@ -132,11 +150,20 @@ nextPlayer(X) :- Z is X + 1, setPlayerTurn(Z).
 setPlayerTurn(X) :- retractall(currentPlayer(_)), assert(currentPlayer(X)).
 
 % Record Data Operations
+% track cards shown to me
 addKnownCard(X) :- retract(unknownCard(X, Y, _)), assert(knownCard(X, Y)). 
+% increment heuristics of unknown cards
+incHeuristics(X) :- knownCard(X, _).
+incHeuristics(X) :- 
+	unknownCard(X, Y, H), 
+	retract(unknownCard(X, Y, H)), 
+	HI is H + 1,
+	assert(unknownCard(X, Y, HI)).
+addShownCard(X) :- currentPlayer(P), assert(shownCard(X, P)).
 
 
 /* My Turn Mechanics */
-myTurn :- myClosestRoom.
+myTurn :- println('Your Turn'), myClosestRoom.
 
 % Ask what room is closest and check whether it's already known
 myClosestRoom :- println('What is the closest room to you?'), read(X), myCheckRoomUnknown(X). 
@@ -150,21 +177,23 @@ printAllCards :- printAllKnownCards, printAllUnknownCards.
 printAllUnknownCards :- printUnknownTitle, printAllUnknownRooms, printAllUnknownWeapons, printAllUnknownPeople.
 printAllKnownCards :- printKnownTitle, printAllKnownRooms, printAllKnownWeapons, printAllKnownPeople.
 
-printAllUnknownRooms :- printRoomsTitle, forall(unknownCard(X, room, _), println(X)), nl.
-printAllUnknownWeapons :- printWeaponsTitle, forall(unknownCard(X, weapon, _), println(X)), nl.
-printAllUnknownPeople :- printPeopleTitle, forall(unknownCard(X, person, _), println(X)), nl.
+printAllUnknownRooms :- printRoomsTitle, forall(unknownCard(X, room, H), printlist([X,H])), nl.
+printAllUnknownWeapons :- printWeaponsTitle, forall(unknownCard(X, weapon, H), printlist([X,H])), nl.
+printAllUnknownPeople :- printPeopleTitle, forall(unknownCard(X, person, H), printlist([X,H])), nl.
 
 printAllKnownRooms :- printRoomsTitle, forall(knownCard(X, room), println(X)), nl.
 printAllKnownWeapons :- printWeaponsTitle, forall(knownCard(X, weapon), println(X)), nl.
 printAllKnownPeople :- printPeopleTitle, forall(knownCard(X, person), println(X)), nl.
 
+printlist([]) :- nl.
+printlist([H|T]) :- write(H), tab(3), printlist(T).
 println(X) :- write(X), nl.
 
-printUnknownTitle :- println('       ***Unknown Cards***').
-printKnownTitle :- println('        ***Known Cards***').
-printRoomsTitle :- println('*=============Rooms===============*'), nl.
-printWeaponsTitle :- println('*============Weapons=============*'), nl.
-printPeopleTitle :- println('*============Suspects=============*'), nl.
+printUnknownTitle :- println('         ***Unknown Cards***').
+printKnownTitle :- println('           ***Known Cards***').
+printRoomsTitle :- println('*==============Rooms==============*'), nl.
+printWeaponsTitle :- println('*=============Weapons=============*'), nl.
+printPeopleTitle :- println('*=============Suspects============*'), nl.
 
 /*
  * Minimal GamePlay
